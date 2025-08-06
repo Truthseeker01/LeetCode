d99fd594a6b53faaee1173f41c09ccba10dc0c07
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2025-08-06 05:31:33 -0500

    update two_sum.py

--- hash_map/two_sum.py
+++ hash_map/two_sum.py
@@ -7,11 +7,16 @@ class Solution:
         Difficulty: Easy
         Link: https://leetcode.com/problems/two-sum/description/
         '''
-        num_map = {}
         
+        num_map = {}
+        #
         for i, n in enumerate(nums):
             diff = target - n
             if diff in num_map:
+                # If the difference is found in the map, return the indices
+                # of the current number and the number that completes the target
                 return [num_map[diff], i]
+            # Store the index of the current number in the map
+            # to use it for future comparisons
             num_map[n] = i
         return []
